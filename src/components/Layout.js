1fee484be86d035eb4c34d94fd2d50c273ba15f7
labdhss <labdhwebd@gmail.com>
2024-07-26 14:36:20 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React, { useContext,useState,useEffect } from 'react'
 import { makeStyles } from '@material-ui/core'
 import { Drawer, Typography, Button } from '@material-ui/core'
 import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
-import { AddCircleOutline, SubjectOutlined, AvTimerOutlined, ChatOutlined, AccountCircleOutlined, CalendarTodayOutlined } from '@material-ui/icons'
+import { AddCircleOutline, SubjectOutlined, AvTimerOutlined, ChatOutlined, AccountCircleOutlined, CalendarTodayOutlined,AddCircleOutlineOutlined } from '@material-ui/icons'
 
 import { AppBar, Toolbar, Avatar } from '@material-ui/core'
 import { useHistory, useLocation } from 'react-router-dom/cjs/react-router-dom.min'
@@ -70,7 +70,8 @@ export function Layout({ children }) {
         {text: 'Pomodoro Timer', icon: <AvTimerOutlined color='secondary' />, path: '/timer'},
         {text: 'Chat', icon: <ChatOutlined color='secondary' />, path: '/chat'},
         {text: 'Calendar', icon: <CalendarTodayOutlined color='secondary' />, path: '/calendar'},
-        {text: 'Profile', icon: <AccountCircleOutlined color='secondary' />, path: '/profile'}
+        {text: 'Profile', icon: <AccountCircleOutlined color='secondary' />, path: '/profile'},
+        {text: 'Calculator', icon: <AddCircleOutlineOutlined color='secondary' />, path: '/Calculator'}
     ];
 
     function signOut() {

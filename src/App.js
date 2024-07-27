ddb0b0bccf652128a67d76a17ab3a0f8ae69dd91
Tom <theodorelim2002@gmail.com>
2024-07-27 10:03:41 +0800

    corrected code

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ProfilePic from './profilePicComponent/profile'
 import { db, profilePicRef } from './firebase'
 import { getDocs, query, where, deleteDoc, doc } from 'firebase/firestore'
 import MyCalendar from './calendarComponents/MyCalendar'
+import Calculator from './CalculatorComponents/Cal'
 
 
 
@@ -55,6 +56,9 @@ function App() {
               </Route>
               <Route exact path='/login/calendar'>
                 <MyCalendar />
+              </Route>
+              <Route exact path='/login/calculator'>
+                <Calculator />
               </Route>
             </Switch>
           </Layout>
